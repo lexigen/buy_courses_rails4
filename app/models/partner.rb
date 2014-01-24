fe34fb871f1857110968f63425d2498076744818
Aleksei Maridashvili <aleksei.maridashvili@altoros.com>
2014-01-24 15:29:48 +0300

    Added validation to partner model

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,5 @@
 class Partner < ActiveRecord::Base
+  validates :title, :email, :address, :phone, presence: true
+  validates :phone, numericality: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
--- spec/features/create_partner_spec.rb
+++ spec/features/create_partner_spec.rb
@@ -1,16 +1,49 @@
 require 'spec_helper'
 
 feature "Create a partner" do |variable|
-  scenario "creating a partner" do |variable|
+  before do
     visit '/'
     click_link "New Partner"
+  end
 
-    fill_in "Title", with: "First Partner"
-    fill_in "Address", with: "Moscow 1"
-    fill_in "Email", with: "example@mail.com"
-    fill_in "Phone", with: "12345678"
-    click_button "Create Partner"
+  context "all required data provided" do
+    scenario "creating a partner" do |variable|
+      fill_in "Title", with: "First Partner"
+      fill_in "Address", with: "Moscow 1"
+      fill_in "Email", with: "example@mail.com"
+      fill_in "Phone", with: "12345678"
+      click_button "Create Partner"
 
-    expect(page).to have_content("The partner has been successfully created.")
+      expect(page).to have_content("The partner has been successfully created.")
+    end
+  end
+
+  context "some required data is missing" do
+    scenario "creating a partner" do |variable|
+      fill_in "Title", with: "First Partner"
+      click_button "Create Partner"
+
+      expect(page).to have_content("The partner has not been created.")
+    end
+  end
+
+  context "phone data is invalid" do
+    scenario "creating a partner" do |variable|
+      fill_in "Phone", with: "dfgh"
+      click_button "Create Partner"
+
+      expect(page).to have_content("The partner has not been created.")
+      expect(page).to have_content("Phone is not a number")
+    end
+  end
+
+  context "phone data is invalid" do
+    scenario "creating a partner" do |variable|
+      fill_in "Email", with: "dfgh"
+      click_button "Create Partner"
+
+      expect(page).to have_content("The partner has not been created.")
+      expect(page).to have_content("Email is invalid")
+    end
   end
 end
